c631d445cb8bc91de52310330f9f2984b5b137a2
nervmouse <wfrtmouse@gmail.com>
2023-09-19 16:06:15 +0800

    update requirement

---
+++ run.py
@@ -0,0 +1,57 @@
+import pydicom
+from os import listdir,walk,rename
+from os.path import isfile, isdir, join
+
+def person_names_callback(dataset, data_element):
+    if data_element.VR == "PN":
+        data_element.value = "anonymous"
+def deID(filepath,ds=None,hash_id=True,items={'InstitutionName':'None','InstitutionalDepartmentName':'None','InstitutionAddress':'None','ReferringPhysicianName':'None','StationName':'AREA51','RETIRED_OtherPatientIDs':'0000000','AdmissionID':'0000000','ProtocolName':'SAMPLE'}):
+    if ds is None:
+        ds=pydicom.dcmread(filepath)
+    
+    for item in items:
+        val=items[item]
+        if hasattr(ds,item):
+            setattr(ds,item,val)
+    ds.walk(person_names_callback)
+    ds.walk(curves_callback)
+    if hash_id:
+      ds.PatientID=md5(ds.PatientID+'deID')
+    #ds.save_as(filepath)
+    return ds
+
+def curves_callback(dataset, data_element):
+    if data_element.tag.group & 0xFF00 == 0x5000:
+        del dataset[data_element.tag]
+
+def reNameDS(dirpath):
+    for root, dns, fns in walk(dirpath):
+        for fn in fns:
+            if 'dcm' in fn:
+              fp= join(root, fn )
+              try:
+                
+                ds=pydicom.dcmread(fp)
+                new_fn=(str(ds.Modality)+".0000"+str(ds.InstanceNumber))[-3:]+'.dcm'
+                new_fp=join(root, new_fn )
+                rename(fp,new_fp)
+              except:
+                print("error"+fp)
+
+import hashlib
+def md5(x,salt=None):
+    if salt is not None:
+        x=x+salt
+    return hashlib.md5(x.encode('utf-8')).hexdigest()
+def DIRdeID(dirpath):
+    dss=[]
+    for root, dns, fns in walk(dirpath):
+        for fn in fns:
+            fp= join(root, fn )
+            ds=deID(fp)
+            
+            ds.save_as(fp)
+            dss.append(ds)
+    return dss
+DIRdeID("./dicom")
+reNameDS("./dicom")
